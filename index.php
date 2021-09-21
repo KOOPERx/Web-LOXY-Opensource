923890ba4ce4390ab2ba162d4c84e4850e0a8e07
KOOPERx <73049524+KOOPERx@users.noreply.github.com>
2021-09-21 23:24:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+		<link rel="shortcut icon" href="https://www.picz.in.th/image/svcQSW" />
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+        <title>LOXY BOT | บอทเพลง</title>
+    </head>
+    <body onkeydown="return false">
+
+         <!-- HEADER -->
+         <header class="l-header">
+            <nav class="nav bd-grid">
+                <div>
+                    <a href="#" class="nav__logo">LOXY</a>
+                </div>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active"><i class='bx bx-home'></i> Home</a></li>
+                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#skills" class="nav__link">Stats</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                <i class='bx bxs-bot' ></i>
+                </div>
+            </nav>
+        </header>
+        <!--<div style="background-image: url('loxy.gif');-webkit-background-size: 100% 100%, auto;">
+            <div class="container">
+                <div class="row justify-content-md-center">
+                    <div class="col col-lg-12">
+                        <canvas></canvas>
+                            <h1 style="color:#FFFFFF;text-align:center;">LNW-DEV</h1>
+                            <h5 style="color:#f8f9fa;text-align:center;">programming</h5>
+                        <canvas></canvas>
+                    </div>
+                </div>
+            </div>
+	    </div> -->
+        <main class="l-main">
+            <!--===== HOME =====-->
+            <section class="home bd-grid" id="home">
+                <div class="home__data">
+                    <h1 class="home__title"><span class="home__title-color">LOXY <i class='bx bxs-bot' ></i></span><br> BOT MUSIC DISCORD</h1> 
+                    <!--<a href="https://discord.com/api/oauth2/authorize?client_id=862690819034972170&permissions=2452748656&scope=bot" class="button"><i class='bx bxs-chevrons-right'></i> เชิญบอท LOXY  </a> <br> <br> -->
+					
+					<!--<a href="https://discord.com/oauth2/authorize?client_id=863000555156275250&permissions=2452748656&scope=bot" class="button"><i class='bx bxs-chevrons-right'></i> เชิญบอท  LOXY2</a> <br> <br> -->
+                    <a href="https://discord.com/oauth2/authorize?client_id=863000555156275250&permissions=2452748656&scope=bot" class="button"><i class='bx bx-bot' ></i> เชิญบอท</a> <br> <br> 
+					<a href="https://discord.gg/ZV5cypHyYS" class="button"><i class='bx bxl-discord'></i> เข้าร่วม Discord</a><br> <br>
+                    <a href="https://www.patreon.com/loxybot" class="button"><i class='bx bxl-patreon'></i> สนับสนุน ( patreon )</a> <br> <br> 
+                    
+
+                </div>
+
+                <div class="home__social">
+                    <a href="https://www.facebook.com/loxybot.xyz" class="home__social-icon"><i class='bx bxl-facebook'></i></a>
+                    <a href="https://www.instagram.com/lnw_sky.exe/" class="home__social-icon"><i class='bx bxl-instagram' ></i></a>
+                    <a href="https://discord.gg/ZV5cypHyYS" class="home__social-icon"><i class='bx bxl-discord' ></i></a>
+                </div>
+
+                <div class="home__img">    
+                    <img src="web.png" alt="">
+                </div>
+
+                
+            </section>
+
+            <!--===== ABOUT =====-->
+            <section class="about section " id="about">
+                <h2 class="section-title">About</h2>
+
+                <div class="about__container bd-grid">
+                    <div class="about__img">
+                        <img src="icon.png" alt="">
+                    </div>
+                    
+                    <div>
+                        <h2 class="about__subtitle">วิธีการใช้งาน</h2>
+                        <p class="about__text">ใช้งานโดยการใช้คำสั่ง  (  l/  )
+                                <br>
+                            🎶 วิธีเล่นเพลง YOUTUBE l/play ( ลิ้งค์เพลง หรือ ชื่อเพลง )
+                            <br><br>
+                            🔎 หาเพลงจาก YouTube ผ่านคำสั่ง l/play ( ลิ้งค์เพลง หรือ ชื่อเพลง )
+                            <br><br>
+                            🎶 วิธีเล่นเพลง  soundcloud l/play ( ลิ้งค์ soundcloud.com )
+                            <br><br>
+                            🔎 วิธีการค้นหาเพลง l/search ( ชื่อเพลง ) จะมีขึ้น 10 ลำดับ <br> ให้ทำการกดเลขตามเพลงที่จะให้เล่น
+                                  จะมีขึ้น 10 ลำดับ ให้ทำการเลือกเลขตามเพลงที่จะให้เล่น
+                                        เช่น  เลือกเพลงเลข 1 ให้ กด1 เเล้วกดส่ง
+                            <br><br>
+                            📃 วิธีการเล่นเพลงในเพลย์ลิสต์ youtube ผ่าน url l/playlist ( ลิ้งเพลย์ลิสต์ )
+                            <br><br>
+                            🔎 วิธีการเล่นเพลงในเพลย์ลิสต์ youtube ผ่านคำค้นหา l/playlist ( คำที่ต้องการค้นหา )
+                            <br><br>
+                                    ดูคำสั่งทั้งหมดโดยกการ l/help
+                            </p>           
+                    </div>                                   
+                </div>
+            </section>
+
+            <!--===== SKILLS =====-->
+            <section class="skills section" id="skills">
+                <h2 class="section-title">STATS</h2>
+
+                <div class="skills__container bd-grid">          
+                    <div>
+                       
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bx-server skills__icon'></i>
+                                <span class="skills__name">SERVER</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">N/A</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bx-user skills__icon'></i>
+                                <span class="skills__name">USERS</span>
+                            </div>
+                            <div class="skills__bar skills__css">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">N/A</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bx-time skills__icon'></i>
+                                <span class="skills__name">UPTIME <i class='bx bx-trending-up'></i></span>
+                            </div>
+                            <div class="skills__bar skills__css">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">99.9%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bx-like skills__icon' ></i>
+                                <span class="skills__name">LIKE</span>
+                            </div>
+                            <div class="skills__bar skills__js">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">96.9%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bx-dislike skills__icon'></i>
+                                <span class="skills__name">DISLIKE</span>
+                            </div>
+                            <div class="skills__bar skills__ux">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">3.1%</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div>              
+                        <img src="server.png" alt="" class="skills__img">
+                    </div>
+                </div>
+            </section>
+
+            <!--===== WORK =====-->
+            
+
+            <!--===== CONTACT =====-->
+            
+        </main>
+
+        <!--===== FOOTER =====-->
+        <footer class="footer">
+            <p class="footer__title">
+            LOXY BOT
+            </p>
+            <div class="footer__social">
+                <a href="https://facebook.com/danunai.sangkachalaw" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
+                <a href="https://www.instagram.com/lnw_sky.exe/" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="https://discord.gg/ZV5cypHyYS" class="footer__icon"><i class='bx bxl-discord' ></i></a>
+            </div>
+            <p><i class='bx bx-been-here'></i> Bangkok , Thailand</p>
+            <p><i class='bx bxs-face'></i> LNW-D3V</p>
+            <p>&#169; 2021 copyright all right reserved</p>
+        </footer>
+
+        <script type="text/javascript">
+// <![CDATA[
+/* No Right Click */
+var message="WWW.LOXYBOT.XYZ";
+function clickIE4()
+{
+	if (event.button==2)
+	{
+		alert(message);
+		return false;
+	}
+}
+function clickNS4(e)
+{
+	if (document.layers||document.getElementById&&!document.all)
+	{
+		if (e.which==2||e.which==3)
+		{
+			alert(message);
+			return false;
+		}
+	}
+}
+if(document.layers)
+{
+	document.captureEvents(Event.MOUSEDOWN);
+	document.onmousedown=clickNS4;
+}
+else if(document.all&&!document.getElementById)
+{
+	document.onmousedown=clickIE4;
+}
+document.oncontextmenu=new Function("alert(message);return false");
+/* End No Right Click */
+
+/* No Highlighting */
+var omitformtags=["input", "textarea", "select"];
+omitformtags=omitformtags.join("|");
+function disableselect(e)
+{
+	if (omitformtags.indexOf(e.target.tagName.toLowerCase())==-1)
+	return false
+}
+function reEnable()
+{
+	return true
+}
+if(typeof document.onselectstart!="undefined")
+{
+	document.onselectstart=new Function ("return false")
+}
+else
+{
+	document.onmousedown=disableselect;
+	document.onmouseup=reEnable;
+}
+/* End  No Highlighting */
+// ]]>
+</script>
+    </body>
+</html>
